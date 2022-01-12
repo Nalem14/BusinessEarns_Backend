b6d78f8a98858d880988064bb5a4c1da3143e0d0
Bucaille Thommy <nalem.14360@gmail.com>
2022-01-13 00:44:21 +0100

    Added Swagger and Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: configService.get("APP_URL", true)
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Business Earn')
+    .setDescription('The Business Earn API documentation.')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    }
+  };
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, customOptions);
 
   await app.listen(configService.get("APP_PORT", 3000));
 }
