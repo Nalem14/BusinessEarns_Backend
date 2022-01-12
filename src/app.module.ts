0d194b3c68d8ba6fd9029f818a159d25781be1e6
Bucaille Thommy <nalem.14360@gmail.com>
2022-01-12 22:00:51 +0100

    Connection to database ok with env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+
 import { User } from './users/user.model';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      models: [
-        User
-      ],
-      autoLoadModels: true,
-      synchronize: true
+    ConfigModule.forRoot({
+      cache: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql',
+        host: configService.get("DB_HOST", "localhost"),
+        port: configService.get("DB_PORT", 3306),
+        username: configService.get("DB_USER", "root"),
+        password: configService.get("DB_PASS", ""),
+        database: configService.get("DB_NAME", "test"),
+        autoLoadModels: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule
   ],
